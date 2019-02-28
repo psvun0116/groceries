fb92065a1886e92ccf902866a90aa82dc984fad5
Paula Vun and Uriel Gonzalez <paula@>
2019-02-27 20:19:44 -0800

    created groceries webpage to input items to uppercase and sort.

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+    var blanks = ["item1", "item2", "item3", "item4"];
+    var answers = [];
+
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val().toUpperCase();
+      answers.push(userInput);
+    });
+
+    answers.sort();
+    $("#result ul").append("<li>" + answers[0] + "</li>");
+    $("#result ul").append("<li>" + answers[1] + "</li>");
+    $("#result ul").append("<li>" + answers[2] + "</li>");
+    $("#result ul").append("<li>" + answers[3] + "</li>");
+    $("#result").show();
+
+    event.preventDefault();
+  });
+});
